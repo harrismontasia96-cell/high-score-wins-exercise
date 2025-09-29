ed1117550dd3d6334ef4876fe85b303bbb8d2e37
montasia harris <mharris@my.yearupunited.com>
2025-09-29 15:19:38 -0400

    string conversion highscore

--- src/main/java/com/pluralsight/HighScoreWins.java
+++ src/main/java/com/pluralsight/HighScoreWins.java
@@ -1,7 +1,29 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class HighScoreWins {
     public static void main(String[] args) {
+        Scanner Game=new Scanner(System.in);
+        System.out.println("Enter Game score :");
+        String score=Game.nextLine();
+        System.out.println("Home:Visitor|21:9 :");
+
+        System.out.println("Enter Game score :");
+        String game=Game.nextLine();
+        System.out.println("Slytherin:Gryffindor|23:59 :");
+
+
+        String HighScoreWins;
+        String[] tokens = Integer.parseInt(Pattern.quote("HighScoreWins));
+        
+
+        String HighScoreWins = Game.nextLine();
+        Double.parseDouble(HighScoreWins);
+
+
+
 
     }
 }
